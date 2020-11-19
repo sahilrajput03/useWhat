21f6d7e128e697cdee2db84ed9533c9fa96a0505
Sahil Rajput <sahilrajput03@gmail.com>
2020-11-19 14:38:19 +0530

    ~sahil

--- index.js
+++ index.js
@@ -5,35 +5,36 @@ let _stateGlobal = [{}, {}];
 let log = console.log;
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export const useStore = (storeName, val) => {
-  const _state = useState({[storeName]: val});
-  const [state, setState] = _state;
-  const [stateGlobal, setStateGlobal] = _stateGlobal;
+export const useWhat = (storeName, val) => {
+	// export const useStore = (storeName, val) => {
+	const _state = useState({[storeName]: val});
+	const [state, setState] = _state;
+	const [stateGlobal, setStateGlobal] = _stateGlobal;
 
-  if (val !== undefined) {
-    stateGlobal[storeName] = state[storeName];
+	if (val !== undefined) {
+		stateGlobal[storeName] = state[storeName];
 
-    setStateGlobal[storeName] = (input) => {
-      if (typeof input === "function") {
-        let callback = input;
-        return setState((state) => ({
-          ...state,
-          [storeName]: callback(state[storeName]),
-        }));
-      }
+		setStateGlobal[storeName] = (input) => {
+			if (typeof input === "function") {
+				let callback = input;
+				return setState((state) => ({
+					...state,
+					[storeName]: callback(state[storeName]),
+				}));
+			}
 
-      return setState({[storeName]: input});
-    };
-  } else if (!setStateGlobal[storeName]) {
-    throw new Error(
-      `Heyy💃💃.., you missed it😬.\n\n\n💡💡Hint: You must give 'defaultValue' 😛😛 at the time of initializing the store. \n\n\nSo, in your case it should be -\n\n\n ️❤️️❤️const [${storeName}, ${
-        "set" + capitalizeFirstLetter(storeName)
-      }] = useStateGlobal('${storeName}', defaultValue)️❤️️❤️${"\n".repeat(5)}`
-    );
-  }
+			return setState({[storeName]: input});
+		};
+	} else if (!setStateGlobal[storeName]) {
+		throw new Error(
+			`Heyy💃💃.., you missed it😬.\n\n\n💡💡Hint: You must give 'defaultValue' 😛😛 at the time of initializing the store. \n\n\nSo, in your case it should be -\n\n\n ️❤️️❤️const [${storeName}, ${
+				"set" + capitalizeFirstLetter(storeName)
+			}] = useStateGlobal('${storeName}', defaultValue)️❤️️❤️${"\n".repeat(5)}`
+		);
+	}
 
-  return [stateGlobal[storeName], setStateGlobal[storeName]];
+	return [stateGlobal[storeName], setStateGlobal[storeName]];
 };
